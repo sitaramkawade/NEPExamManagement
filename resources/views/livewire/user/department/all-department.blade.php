63dd38e8e12cfd1f0f0e81683d168fb6258e0d83
Hruta0905 <152952484+Hruta0905@users.noreply.github.com>
2024-02-20 11:18:17 +0530

    Breadcump issue solve

--- resources/views/livewire/user/department/all-department.blade.php
+++ resources/views/livewire/user/department/all-department.blade.php
@@ -19,7 +19,7 @@
         <x-breadcrumb.breadcrumb>
             <x-breadcrumb.link route="user.dashboard" name="Dashboard" />
             <x-breadcrumb.link name="Department's" />
-            </x-breadcrumb.link>
+        </x-breadcrumb.breadcrumb>
             <x-card-header heading=" All Department's">
                 <x-add-btn wire:click="setmode('add')" />
             </x-card-header>
--- resources/views/livewire/user/exam-time-table/all-exam-time-table.blade.php
+++ resources/views/livewire/user/exam-time-table/all-exam-time-table.blade.php
@@ -22,7 +22,7 @@
         <x-breadcrumb.breadcrumb>
             <x-breadcrumb.link route="user.dashboard" name="Dashboard" />
             <x-breadcrumb.link name="Exam Time Table's" />
-            </x-breadcrumb.link>
+        </x-breadcrumb.breadcrumb>
             <x-card-header heading=" All Exam Time Table's">
 
             </x-card-header>
