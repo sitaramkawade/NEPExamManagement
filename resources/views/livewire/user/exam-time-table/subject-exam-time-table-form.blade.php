bc6faad276c5bb833cfdea3309cbef25ad125c1e
maxpro <ashutoshpuri2000@gmail.com>
2024-02-28 15:06:37 +0530

    subject wise timetable add method by ashutosh

--- resources/views/livewire/user/exam-time-table/subject-exam-time-table-form.blade.php
+++ resources/views/livewire/user/exam-time-table/subject-exam-time-table-form.blade.php
@@ -68,16 +68,16 @@
                     <x-table.td> {{ isset($exampatternclass->patternclass->pattern->pattern_name) ? $exampatternclass->patternclass->pattern->pattern_name : '-' }} {{ isset($exampatternclass->patternclass->courseclass->classyear->classyear_name) ? $exampatternclass->patternclass->courseclass->classyear->classyear_name : '-' }} {{ isset($exampatternclass->patternclass->courseclass->course->course_name) ? $exampatternclass->patternclass->courseclass->course->course_name : '-' }} </x-table.td>
                     <x-table.td>
                         <div class="px-5 py-2 text-sm text-gray-600 dark:text-gray-400">
-                            <x-input-label for="examdates.{{ $exampatternclass->id}}.{{ $subjectbucket->id }}"  />
-                            <x-text-input id="examdates.{{ $exampatternclass->id}}.{{ $subjectbucket->id }}" type="date" wire:model="examdates.{{ $exampatternclass->id}}.{{ $subjectbucket->id }}"  name="examdates.{{ $exampatternclass->id}}.{{ $subjectbucket->id }}" class="w-full mt-1"  />
-                            <x-input-error :messages="$errors->get('examdates.{$exampatternclass->id}.{ $subjectbucket->id }')" class="mt-1" />
+                            <x-input-label for="examdates.{{ $exampatternclass->id }}"  />
+                            <x-text-input id="examdates.{{ $exampatternclass->id }}" type="date" wire:model="examdates.{{ $exampatternclass->id }}"  name="examdates.{{ $exampatternclass->id }}" class="w-full mt-1"  />
+                            <x-input-error :messages="$errors->get('examdates.{$exampatternclass->id}')" class="mt-1" />
                         </div>
                     </x-table.td>
 
                     <x-table.td>
-                        <div class="px-5 py-2  text-sm text-gray-600 dark:text-gray-400">
-                            <x-input-label for="timeslot_ids.{{ $exampatternclass->id}}" />
-                            <x-input-select id="timeslot_ids.{{ $exampatternclass->id}}" wire:model="timeslot_ids.{{ $exampatternclass->id}}" name="timeslot_ids.{{ $exampatternclass->id}}" class="text-center w-full mt-1">
+                        <div class="px-5 py-2  text-sm text-gray-600 dark:text-gra-400">
+                            <x-input-label for="timeslot_ids.{{ $exampatternclass->id }}" />
+                            <x-input-select id="timeslot_ids.{{ $exampatternclass->id }}" wire:model="timeslot_ids.{{ $exampatternclass->id }}" name="timeslot_ids.{{ $exampatternclass->id }}" class="text-center w-full mt-1">
                                 <x-select-option class="text-start" hidden> -- Select Time Slot -- </x-select-option>
                                 @forelse ($timeslots as $timeid=>$timeslot)
                                 <x-select-option wire:key="{{ $timeid}}" value="{{ $timeid }}" class="text-start"> {{ $timeslot }} </x-select-option>
