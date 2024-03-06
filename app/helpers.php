8bcca409b9de823ca23845a4c43862175431ffe9
maxpro <ashutoshpuri2000@gmail.com>
2024-03-06 20:42:52 +0530

    V100 helper function  get_exam_form_fees

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,16 @@
 <?php
 
-use App\Models\Course;
-use App\Models\Subject;
-use App\Models\Departmentprefix;
-use App\Models\Courseclass;
 use App\Models\Exam;
+use App\Models\Course;
+use App\Models\Setting;
 use App\Models\Student;
+use App\Models\Subject;
+use App\Models\Courseclass;
+use App\Models\Examfeeview;
 use App\Models\PatternClass;
+use App\Models\Departmentprefix;
+use App\Models\Exampatternclass;
+use App\Models\Currentclassstudent;
 use Illuminate\Support\Facades\Auth;
 
 // Getting Pattern Class Name
@@ -95,3 +99,191 @@ if (!function_exists('get_student_current_sem'))
     }
 }
 
+
+// Getting Student Exam Fees
+if (!function_exists('get_exam_form_fees')) 
+{
+    function get_exam_form_fees($regular_subject_data,$backlog_subject_data)
+    {   
+        $fee_courses = collect();
+        $patternclass_id=null;
+        $internal_count = 0;
+        $backlog_count = 0;
+        $project_count = 0;
+        $evs_count = 0;
+        $sem_count = 0;
+        $sems = [];
+        $setting = Setting::first();
+        $student=Auth::guard('student')->user();
+        $statement_of_marks_is_year_wise = isset($setting->statement_of_marks_is_year_wise) ? $setting->statement_of_marks_is_year_wise : 0;
+        $pattern_class_count = Currentclassstudent::where('student_id',$student->id)->pluck('patternclass_id')->count();
+        $current_class_student_last_entry = $student->currentclassstudents->last();
+        if($current_class_student_last_entry)
+        {
+            $patternclass_id=$current_class_student_last_entry->patternclass_id;
+        }else
+        {
+            $patternclass_id=$student->patternclass_id;
+        }
+
+
+
+        if (isset($backlog_subject_data)) {
+            $backlog_count = count($backlog_subject_data);
+        }
+
+        $regular_and_backlog_subjects = collect($regular_subject_data)->merge($backlog_subject_data);
+
+        foreach ($regular_and_backlog_subjects as $subject) {
+            if ($subject->subjectcategory->subjectcategory == "Project") {
+                $project_count++;
+            }
+
+            if ($subject->subjectcategory->subjectcategory == "EVS") {
+                $evs_count++;
+            }
+
+            if ($subject->subject_sem) {
+                $temp[] = $subject->subject_sem;
+                $sems = array_values(array_unique($temp));
+            }
+
+            if ($subject->subject_type == "I" || $subject->subject_type == "IP" || $subject->subject_type == "IE" || $subject->subject_type == "IG" || $subject->subject_type == "IEP" || $subject->subject_type == "IEG") {
+                $internal_count++;
+            }
+        }
+
+        $sem_count = count($sems);
+
+        $exam = Exam::where('status', 1)->first();
+        if ($exam) {
+            $exam_pattern_class = Exampatternclass::where('patternclass_id', $patternclass_id)->where('exam_id', $exam->id)->first();
+            $student_course_fees = Examfeeview::where('patternclass_id', $patternclass_id)->where('active_status', 1)->get();
+            if ($student_course_fees) {
+                $fee_data = [];
+                foreach ($student_course_fees as $course_fee) {
+                    if ($course_fee->fee_name == "Form Fee") {
+                        $fee_courses->push($course_fee);
+                    }
+                    if ($course_fee->fee_name == "Exam Fee") {
+                        $fee_courses->push($course_fee);
+                    }
+
+                    if ($course_fee->fee_name == "CAP Fee") {
+                        if ($sem_count) {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = ($updated_course_fee->fee * $sem_count);
+                            $fee_courses->push($updated_course_fee);
+                        }
+                    }
+
+                    if ($course_fee->fee_name == "Statement of Marks Fee") {
+                        // Apply Year Wise
+                        if ($statement_of_marks_is_year_wise) {
+                            if ($pattern_class_count) {
+                                $updated_course_fee = clone $course_fee;
+                                $updated_course_fee->fee = ($updated_course_fee->fee * $pattern_class_count);
+                                $fee_courses->push($updated_course_fee);
+                            }
+                        } else {
+                            if ($sem_count) {
+                                $updated_course_fee = clone $course_fee;
+                                $updated_course_fee->fee = ($updated_course_fee->fee * $sem_count);
+                                $fee_courses->push($updated_course_fee);
+                            }
+                        }
+                    }
+
+                    if ($course_fee->fee_name == "Passing Certificate Fee") {
+                        $fee_courses->push($course_fee);
+                    }
+
+                    if ($course_fee->fee_name == "Project Fee/Dissertation") {
+                        if ($project_count) {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = ($updated_course_fee->fee * $project_count);
+                            $fee_courses->push($updated_course_fee);
+                        } else {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = 0;
+                            $fee_courses->push($updated_course_fee);
+                        }
+                    }
+
+                    if ($course_fee->fee_name == "EVS Fee") {
+                        if ($evs_count) {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = ($updated_course_fee->fee * $evs_count);
+                            $fee_courses->push($updated_course_fee);
+                        } else {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = 0;
+                            $fee_courses->push($updated_course_fee);
+                        }
+                    }
+
+                    if ($course_fee->fee_name == "Internal Marks Fee") {
+                        if ($internal_count) {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = ($updated_course_fee->fee * $internal_count);
+                            $fee_courses->push($updated_course_fee);
+                        } else {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee = 0;
+                            $fee_courses->push($updated_course_fee);
+                        }
+                    }
+
+                    if ($course_fee->fee_name == "Departmental Fee") {
+                        $fee_courses->push($course_fee);
+                    }
+
+                    if ($course_fee->fee_name == "Transcript Fee") {
+                        $fee_courses->push($course_fee);
+                    }
+
+                    if ($exam_pattern_class) {
+                        if ($course_fee->fee_name == "Late Fee") {
+                            if (isset($exam_pattern_class->latefee_date)) {
+                                $late_fee_date = date('Y-m-d', strtotime($exam_pattern_class->latefee_date));
+                                $today_date = date('Y-m-d');
+                                if ($today_date > $late_fee_date) {
+                                    $fee_courses->push($course_fee);
+                                } else {
+                                    $updated_course_fee = clone $course_fee;
+                                    $updated_course_fee->fee = 0;
+                                    $fee_courses->push($updated_course_fee);
+                                }
+                            }
+                        }
+
+                        if ($course_fee->fee_name == "Fine Fee") {
+                            if (isset($exam_pattern_class->finefee_date)) {
+                                $fine_fee_date = date('Y-m-d', strtotime($exam_pattern_class->finefee_date));
+                                $today_date = date('Y-m-d');
+                                if ($today_date > $fine_fee_date) {
+                                    $fee_courses->push($course_fee);
+                                } else {
+                                    $updated_course_fee = clone $course_fee;
+                                    $updated_course_fee->fee = 0;
+                                    $fee_courses->push($updated_course_fee);
+                                }
+                            }
+                        }
+                    }
+
+                    if ($course_fee->fee_name == "Backlog Subject Exam Fee") {
+                        if ($backlog_count) {
+                            $updated_course_fee = clone $course_fee;
+                            $updated_course_fee->fee_name = "Backlog Fee";
+                            $updated_course_fee->fee = ($updated_course_fee->fee * $backlog_count);
+                            $fee_courses->push($updated_course_fee);
+                        }
+                    }
+                }
+            }
+        }
+
+        return $fee_courses;
+    }
+}
