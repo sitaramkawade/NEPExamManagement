162256bff4b74f1c1c29917fa46499e632c8b68c
maxpro <ashutoshpuri2000@gmail.com>
2024-03-11 22:10:09 +0530

    v107 Select 2 Style Added

--- resources/views/components/select2/select.blade.php
+++ resources/views/components/select2/select.blade.php
@@ -1,20 +1,20 @@
 @props([ 'slot'=>false ,'id'=>rand(1111, 9999) , 'name'=>'no_name'])
 
 <div wire:ignore>
-    <select  id="{{ $id }}"    {!! $attributes->merge(['class' => 'xyz ']) !!} >
+    <select  id="{{ $id }}"    {!! $attributes->merge(['class' => '']) !!} >
        {{$slot}}
     </select>
 </div>
 
-@section('scripts')
-    <script>
-        $(document).ready(function () {
-            $('#{{ $id }}').select2();
-            $('#{{ $id }}').on('change', function (e) {
-                var data = $('#{{ $id }}').select2("val");
-            @this.set('{{ $name }}', data);
-            });
-        });
-    </script>
-@endsection
 
+@push('scripts')
+<script>
+    $(document).ready(function () {
+        $('#{{ $id }}').select2();
+        $('#{{ $id }}').on('change', function (e) {
+            var data = $('#{{ $id }}').select2("val");
+        @this.set('{{ $name }}', data);
+        });
+    });
+</script>   
+@endpush
